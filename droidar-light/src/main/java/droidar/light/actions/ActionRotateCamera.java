315f6332856c60065683233c18138978efce45c1
Johannes Rudolph <jojo.rudolph@gmail.com>
2016-02-14 11:52:54 +0100

    Rename ActionRotateCameraBuffered -> ActionRotateCamera (there's no buffering of rotation going on)

--- droidar-light/src/main/java/droidar/light/actions/ActionRotateCamera.java
+++ droidar-light/src/main/java/droidar/light/actions/ActionRotateCamera.java
@@ -3,10 +3,10 @@ package droidar.light.actions;
 import droidar.light.gl.GLCamera;
 import droidar.light.sensors.RotationMatrixListener;
 
-public class ActionRotateCameraBuffered implements RotationMatrixListener {
+public class ActionRotateCamera implements RotationMatrixListener {
     private final GLCamera camera;
 
-    public ActionRotateCameraBuffered(GLCamera targetCamera) {
+    public ActionRotateCamera(GLCamera targetCamera) {
         camera = targetCamera;
     }
 
